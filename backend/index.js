72ad60524c0989789011b496289db4663fb87032
Ayan041 <140093089+Ayan041@users.noreply.github.com>
2025-10-12 21:46:27 +0530

    changes updated

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dbConnect();
 
 // ✅ Use CORS middleware
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from your React frontend
+    origin: process.env.FRONTEND_URL, // Allow requests from your React frontend
     credentials: true
 }));
 
